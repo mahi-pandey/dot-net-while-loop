be474e6f1fbee09fd847bb4e58d8cc3583e16a9f
Mahima Pandey <mahipandey2206@gmail.com>
2025-06-13 12:52:50 +0530

    Add files via upload

---
+++ whileloop/whileloop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace LoopExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 10)
+            {
+                Console.WriteLine("patients are admitted");
+                i++;
+            }
+            int x = 1;
+            do
+            {
+                Console.WriteLine(x);
+                Console.WriteLine("Mahima");
+                x++;
+
+            }
+            while (x > 10);
+
+
+        }
+    }
+}
